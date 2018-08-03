32ae1c26a5e4fd546d1d9b2f85c42bc2cd2927a0
ljb <837187695@qq.com>
2018-08-03 22:09:22 +0800

    增加一个抽象工厂模式的存储方式

---
+++ baselibrary/src/main/java/com/ljb/baselibrary/storagefactory/AbstractDiskIOHandler.java
@@ -0,0 +1,120 @@
+package com.ljb.baselibrary.storagefactory;
+
+import java.io.InputStream;
+
+/**
+ * Author      :ljb
+ * Date        :2018/8/3
+ * Description :
+ */
+public abstract class AbstractDiskIOHandler implements IOHandler{
+
+   public  abstract void save(String key, InputStream inputStream);
+public abstract InputStream get(String key);
+    public abstract void  clear(String key);
+    @Override
+    public void save(String key, String value) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void save(String key, double value) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void save(String key, int value) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void save(String key, long value) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void save(String key, boolean value) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void save(String key, Object value) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void save(String key, float value) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public String getString(String key) {
+        throw new RuntimeException("不支持");
+
+    }
+
+    @Override
+    public double getDouble(String key, double defaultValue) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public int getInt(String key, int defaultValue) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public long getLong(String key, long defaultValue) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public boolean getBoolean(String key, boolean defaultValue) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public Object getObject(String key, Object defaultValue) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public float getFloat(String key, float defaultValue) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void clearString(String key) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void clearDouble(String key) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void clearInt(String key) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void clearLong(String key) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void clearBoolean(String key) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void clearObject(String key) {
+        throw new RuntimeException("不支持");
+    }
+
+    @Override
+    public void clearFloat(String key) {
+        throw new RuntimeException("不支持");
+    }
+}
---
+++ baselibrary/src/main/java/com/ljb/baselibrary/storagefactory/DiskLruIOHandler.java
@@ -0,0 +1,105 @@
+package com.ljb.baselibrary.storagefactory;
+
+import android.content.Context;
+
+import com.jakewharton.disklrucache.DiskLruCache;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+/**
+ * Author      :ljb
+ * Date        :2018/8/3
+ * Description : 磁盘缓存
+ */
+public class DiskLruIOHandler extends AbstractDiskIOHandler {
+    private DiskLruCache mDiskLruCache;
+
+    public DiskLruIOHandler(Context context) {
+        File dir = context.getExternalFilesDir("DiskLruCache");
+        int MAX_SIZE = 10 * 1024 * 1024;//10MB
+        try {
+            mDiskLruCache = DiskLruCache.open(dir, 1, 1, MAX_SIZE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void save(String key, InputStream inputStream) {
+        if (mDiskLruCache == null) return;
+        OutputStream outputStream;
+        try {
+            DiskLruCache.Editor edit = mDiskLruCache.edit(key);
+            if (edit != null) {
+                outputStream = edit.newOutputStream(0);
+                if (write(outputStream, inputStream)) {
+                    edit.commit();
+                } else {
+                    edit.abort();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public InputStream get(String key) {
+        if (mDiskLruCache == null) return null;
+        try {
+            DiskLruCache.Snapshot snapshot = mDiskLruCache.get(key);
+            if (snapshot != null) {
+                return snapshot.getInputStream(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void clear(String key) {
+        if(mDiskLruCache==null) return;
+        try {
+            mDiskLruCache.remove(key);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private boolean write(OutputStream outputStream, InputStream inputStream) {
+        if (outputStream == null || inputStream == null) return false;
+        try {
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, len);
+            }
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            cloase(inputStream);
+            cloase(outputStream);
+        }
+        return false;
+    }
+
+    private void cloase(Closeable io) {
+        if (io != null) {
+            try {
+                io.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
