b2429f0ce0f88a25f4e2ec53639b64db49954e20
Administrator <837187695>
2018-10-23 15:23:13 +0800

    增加一个图片剪辑第三方框架

--- baselibrary/src/main/java/com/ljb/baselibrary/utils/InstallUitls.java
+++ baselibrary/src/main/java/com/ljb/baselibrary/utils/InstallUitls.java
@@ -1,13 +1,10 @@
 package com.ljb.baselibrary.utils;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.FileProvider;
 
 import java.io.File;
 
@@ -32,7 +29,7 @@ public class InstallUitls {
                 installApk(context, apkFile);
             }else{
                 if(context instanceof Activity){
-                    ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.REQUEST_INSTALL_PACKAGES}, 10010);
+//                    ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.REQUEST_INSTALL_PACKAGES}, 10010);
                 }
             }
         } else {
@@ -41,13 +38,53 @@ public class InstallUitls {
 
     }
 
+    /**
+     *
+     * 用得uCrop剪辑框架
+     */
+//    public void cropRawPhoto(Uri uri) {  //这个uri是拍照后保存下来的图片文件路径
+//        mCropFile = new File(getExternalFilesDir("crop_img").getAbsolutePath(), System.currentTimeMillis() + ".jpg");
+//        mCropFile.getParentFile().mkdirs();
+//        // 修改配置参数（我这里只是列出了部分配置，并不是全部）
+//        UCrop.Options options = new UCrop.Options();
+//        // 修改标题栏颜色
+//        options.setToolbarColor(getResources().getColor(R.color.colorPrimary));
+//        //设置裁剪图片可操作的手势
+//        options.setAllowedGestures(UCropActivity.SCALE, UCropActivity.ROTATE, UCropActivity.ALL);
+//        // 修改状态栏颜色
+//        options.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+//        // 隐藏底部工具
+//        options.setHideBottomControls(true);
+//        //是否能调整裁剪框
+//        options.setShowCropFrame(true);
+//        // 图片格式
+//        options.setCompressionFormat(Bitmap.CompressFormat.JPEG);
+//        // 设置图片压缩质量
+//        options.setCompressionQuality(100);
+//        // 是否让用户调整范围(默认false)，如果开启，可能会造成剪切的图片的长宽比不是设定的
+//        // 如果不开启，用户不能拖动选框，只能缩放图片
+//        options.setFreeStyleCropEnabled(true);
+//
+//        // 设置源uri及目标uri
+//        UCrop.of(uri, Uri.fromFile(mCropFile))
+//                // 长宽比
+//                .withAspectRatio(1, 1)
+//                // 图片大小
+//                .withMaxResultSize(200, 200)
+//                // 配置参数
+//                .withOptions(options)
+//                .start(this);
+//
+//    }
+
     public static void installApk(Context context, File apkFile) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         //判读版本是否在7.0以上
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Uri apkUri = FileProvider.getUriForFile(context, context.getPackageName() + ".provider", apkFile);
+           //  apkFile：是.apk文件
+//            Uri apkUri = FileProvider.getUriForFile(context, context.getPackageName() + ".provider", apkFile);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+//            intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
         } else {
             intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
         }
